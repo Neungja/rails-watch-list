7fadd08d2bafe91b3a6ab235ddbe801fcfeff0c2
ayakookawa <three.peach.0524@gmail.com>
2024-07-11 21:24:41 +0900

    push push

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,14 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
-    tmdb_api = TheMovieDatabaseApi.new
-    @data_json = tmdb_api.top_rated
-    @god_father = tmdb_api.by_movie_id(238)
-
-    @sister_act = tmdb_api.by_movie_id(2005)
   end
 
   def show
     @list = List.find(params[:id])
+    tmdb_api = TheMovieDatabaseApi.new
+    @data_json = tmdb_api.top_rated
+    @god_father = tmdb_api.by_movie_id(238)
+    @sister_act = tmdb_api.by_movie_id(2005)
   end
 
   def new
